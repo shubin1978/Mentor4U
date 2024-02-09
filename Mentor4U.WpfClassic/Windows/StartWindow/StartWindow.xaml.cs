ea166c2a8edbc3fad80c42f6256373f7e2994ef3
yury <shubin1978@omnicommdv.ru>
2024-02-09 14:35:37 +0900

    Add StartWindow.xaml

---
+++ Mentor4U.WpfClassic/Windows/StartWindow/StartWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Mentor4U.WpfClassic.Windows.StartWindow;
+
+public partial class StartWindow : Window
+{
+    public StartWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
