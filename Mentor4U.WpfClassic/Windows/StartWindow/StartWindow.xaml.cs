b8d3eda578cd71a905fc7cc983264790cb2a1bf9
yury <shubin1978@omnicommdv.ru>
2024-02-10 08:43:38 +0900

    Добаление обработчика на закрытие окна

--- Mentor4U.WpfClassic/Windows/StartWindow/StartWindow.xaml.cs
+++ Mentor4U.WpfClassic/Windows/StartWindow/StartWindow.xaml.cs
@@ -7,6 +7,18 @@ public partial class StartWindow : Window
     public StartWindow()
     {
         InitializeComponent();
+        
+        this.Closing += (_, e) =>
+        {
+            var result = MessageBox.Show(
+                owner: this, messageBoxText: "Вы действительно хотите закрыть приложение?",
+                caption: App.Current.Resources["AppTitle"].ToString(), MessageBoxButton.YesNo,
+                icon: MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        };
     }
 
     private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
